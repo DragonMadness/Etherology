ef29c2fa2c9c1717eaa5004c9cb4df3390630140
Feytox <73577753+feytox@users.noreply.github.com>
2025-01-21 20:06:30 +0500

    feature: #46

--- src/main/java/ru/feytox/etherology/item/StreamKeyItem.java
+++ src/main/java/ru/feytox/etherology/item/StreamKeyItem.java
@@ -1,17 +1,16 @@
 package ru.feytox.etherology.item;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
 import net.minecraft.item.ToolItem;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
+import net.minecraft.util.ItemScatterer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
-import net.minecraft.util.math.random.Random;
 import org.jetbrains.annotations.Nullable;
 import ru.feytox.etherology.block.etherealChannel.EtherealChannel;
 import ru.feytox.etherology.registry.block.EBlocks;
@@ -26,10 +25,10 @@ public class StreamKeyItem extends ToolItem {
 
     @Override
     public ActionResult useOnBlock(ItemUsageContext context) {
-        ActionResult result = tryUseOnChannel(context);
+        var result = tryUseOnChannel(context);
         if (result == null) return super.useOnBlock(context);
 
-        Random random = context.getWorld().getRandom();
+        var random = context.getWorld().getRandom();
         context.getWorld().playSound(null, context.getBlockPos(), EtherSounds.RATCHET, SoundCategory.BLOCKS, 0.8f, random.nextFloat()*0.2f+0.9f);
         return result;
     }
@@ -38,17 +37,38 @@ public class StreamKeyItem extends ToolItem {
     private ActionResult tryUseOnChannel(ItemUsageContext context) {
         if (!(context.getWorld() instanceof ServerWorld world)) return null;
 
-        BlockPos pos = context.getBlockPos();
-        BlockState state = world.getBlockState(pos);
+        var pos = context.getBlockPos();
+        var state = world.getBlockState(pos);
         if (!(state.getBlock() instanceof EtherealChannel)) return null;
 
-        PlayerEntity player = context.getPlayer();
-        boolean isPlayer = player != null;
-        boolean isVertical = isPlayer && player.isSneaking();
-        ItemStack stack = context.getStack();
+        var player = context.getPlayer();
+        var isPlayer = player != null;
+        var isVertical = isPlayer && player.isSneaking();
+        var stack = context.getStack();
 
-        Direction pipeFacing = state.get(EtherealChannel.FACING);
-        Direction playerFacing = context.getHorizontalPlayerFacing();
+        if (state.get(EtherealChannel.IN_CASE))
+            removeChannelCase(context, world, state, pos);
+        else
+            rotateChannel(context, world, state, isVertical, pos);
+
+        var hand = context.getHand();
+        if (isPlayer) stack.damage(1, player, PlayerEntity.getSlotForHand(hand));
+        else stack.damage(1, world, null, item -> {});
+
+        return ActionResult.SUCCESS;
+    }
+
+    private static void removeChannelCase(ItemUsageContext context, ServerWorld world, BlockState state, BlockPos pos) {
+        state = state.with(EtherealChannel.IN_CASE, false);
+        world.setBlockState(pos, state, Block.NOTIFY_ALL);
+
+        var itemPos = pos.add(context.getSide().getVector());
+        ItemScatterer.spawn(world, itemPos.getX(), itemPos.getY(), itemPos.getZ(), EBlocks.CHANNEL_CASE.asItem().getDefaultStack());
+    }
+
+    private static void rotateChannel(ItemUsageContext context, ServerWorld world, BlockState state, boolean isVertical, BlockPos pos) {
+        var pipeFacing = state.get(EtherealChannel.FACING);
+        var playerFacing = context.getHorizontalPlayerFacing();
         if (isVertical) {
             pipeFacing = pipeFacing.getOffsetY() == 0 ? Direction.UP : pipeFacing.getOpposite();
         }
@@ -57,12 +77,6 @@ public class StreamKeyItem extends ToolItem {
         }
 
         state = EBlocks.ETHEREAL_CHANNEL.getChannelState(world, state.with(EtherealChannel.FACING, pipeFacing), pos);
-        world.setBlockState(pos, state, EtherealChannel.NOTIFY_ALL);
-
-        Hand hand = context.getHand();
-        if (isPlayer) stack.damage(1, player, PlayerEntity.getSlotForHand(hand));
-        else stack.damage(1, world, null, item -> {});
-
-        return ActionResult.SUCCESS;
+        world.setBlockState(pos, state, Block.NOTIFY_ALL);
     }
 }
